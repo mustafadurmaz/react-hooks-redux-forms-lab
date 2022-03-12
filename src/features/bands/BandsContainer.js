1bc8e2b9410f79def89f671697e386f98fece0d8
Mustafa Durmaz <mdmz_001@hotmail.com>
2022-03-12 20:38:06 +0300

    Done

--- src/features/bands/BandsContainer.js
+++ src/features/bands/BandsContainer.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import BandInput from "./BandInput";
+import { bandAdded } from "./bandsSlice";
 
 function BandsContainer() {
-  return <div>BandsContainer</div>;
+  const bands = useSelector((state) => state.bands.entities);
+  const dispatch = useDispatch();
+  const bandItems = bands.map((band) => <li key={band}>{band}</li>);
+  const handleBandSubmit = (band) => {
+    dispatch(bandAdded(band));
+  };
+
+  return (
+    <div>
+      <BandInput onBandSubmit={handleBandSubmit} />
+      <ul>{bandItems}</ul>
+    </div>
+  );
 }
 
 export default BandsContainer;
--- src/features/bands/bandsSlice.js
+++ src/features/bands/bandsSlice.js
@@ -6,10 +6,54 @@ const bandsSlice = createSlice({
     entities: [],
   },
   reducers: {
-    // create reducer methods
+    bandAdded(state, action) {
+      state.entities.push(action.payload);
+    },
   },
 });
 
 // export the action creators
-
+export const { bandAdded } = bandsSlice.actions;
 export default bandsSlice.reducer;
+
+// import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+// import { act } from "@testing-library/react";
+
+// export const fetchCats = createAsyncThunk("cats/fetchCats", () => {
+//   // return a Promise containing the data we want
+//   return fetch("https://learn-co-curriculum.github.io/cat-api/cats.json")
+//     .then((response) => response.json())
+//     .then((data) => data.images);
+// });
+
+// const catsSlice = createSlice({
+//   name: "cats",
+//   initialState: {
+//     entities: [], // array of cats
+//     status: "idle", // loading state
+//   },
+//   reducers: {
+//     catAdded(state, action) {
+//       // using createSlice lets us mutate state!
+//       state.entities.push(action.payload);
+//     },
+//     catUpdated(state, action) {
+//       const cat = state.entities.find((cat) => cat.id === action.payload.id);
+//       cat.url = action.payload.url;
+//     },
+//   },
+//   extraReducers: {
+//     // handle async actions: pending, fulfilled, rejected (for errors)
+//     [fetchCats.pending](state) {
+//       state.status = "loading";
+//     },
+//     [fetchCats.fulfilled](state, action) {
+//       state.entities = action.payload;
+//       state.status = "idle";
+//     },
+//   },
+// });
+
+// export const { catAdded, catUpdated } = catsSlice.actions;
+
+// export default catsSlice.reducer;
