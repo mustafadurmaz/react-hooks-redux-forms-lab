2069c75067397d6e66fa5336db0faf0308c76ff1
Mustafa Durmaz <mdmz_001@hotmail.com>
2022-03-12 20:37:21 +0300

    commit tree 13bd9e0473d69362d3abdd1b283ea13d6e28004d

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import BandInput from "./features/bands/BandInput";
 import BandsContainer from "./features/bands/BandsContainer";
 
 function App() {
   return (
     <div className="App">
+      <BandInput/>
       <BandsContainer />
     </div>
   );
--- src/features/bands/BandInput.js
+++ src/features/bands/BandInput.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
-function BandInput() {
-  return <div>Band Input</div>;
+function BandInput({onBandSubmit}) {
+  const [name, setName] = useState("");
+
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onBandSubmit(name);
+    setName("");
+  };
+  return(
+    <div>Band Input
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Band Name</label>
+        <input id="name" value={name} onChange={handleChange} />
+        <input type="submit" value="Add Band" />
+      </form>
+    </div>
+  )
 }
 
 export default BandInput;
